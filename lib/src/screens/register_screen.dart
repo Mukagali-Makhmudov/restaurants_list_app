937eed5131dba702c05419ac7037babdc2046286
Mukagali-Makhmudov <mukagali.mahmudov@gmail.com>
2021-10-09 13:51:53 +0300

    commit3

--- lib/src/screens/register_screen.dart
+++ lib/src/screens/register_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/cupertino.dart';
 
-class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
